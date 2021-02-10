48995275a5053b78f14c19638459676b1998347f
zili <erik.zilinsky@icontest.hu>
2021-02-10 09:02:27 +0100

    comment added

--- 1.py
+++ 1.py
@@ -7,6 +7,7 @@ f = open("1_input",'r',encoding = 'utf-8')
 for line in f:
     expenses_list.append(int(line))
 
+#List comprehensions 
 first_part = [x for x in list(combinations(expenses_list, 2)) if sum(x) == 2020][0]
 print(f'first solution: {first_part[0] * first_part[1]}')
 
